f93994ba39e3e9c8291e91bdf1b5cef9a4dc259d
Ahmed Khalil <Ahmed.ismail.khalil.1994@gmail.com>
2023-05-02 19:55:14 -0700

    test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 /**
  * print the elements using print_listint
- * h makes us print linjed list
- * we should return the number of the nodes at the end
+ * @h makes us print linjed list
+ * Return: we should return the number of the nodes at the end
  */
 size_t print_listint(const listint_t *h)
 {
- size_t num = 0;
+size_t num = 0;
 
- while (h)
- {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
+while (h)
+{
+printf("%d\n", h->n);
+num++;
+h = h->next;
+}
 
- return (num);
+return (num);
 }
