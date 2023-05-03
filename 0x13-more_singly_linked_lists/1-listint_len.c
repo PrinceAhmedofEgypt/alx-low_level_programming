25ab1f576f791d9fbe9677da962203348ca4b732
Ahmed Khalil <Ahmed.ismail.khalil.1994@gmail.com>
2023-05-02 19:43:19 -0700

    first tasks

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * linked list elements returned using listint_len
+ * traverse linked list type using h
+ * nodes number is returned
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t num = 0;
+while (h)
+{
+num++;
+h = h->next;
+}
+return (num);
+}
